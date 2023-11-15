b40dc90c1a31ce872a17ac6a2af399ac66533907
Josseluu <jslslpz@myuax.com>
2023-11-15 19:41:51 +0100

    Initial commit

--- Ciencia/Física.cpp
+++ Ciencia/Física.cpp
@@ -1,3 +1,14 @@
-//
-// Created by Jose luis Lopez lopez on 15/11/23.
-//
+#include <iostream>
+namespace Ciencia {
+    namespace Fisica {
+        const double c = 299792458;
+        double calcularEnergia(double masa) {
+            return masa * c * c;
+        }
+    }
+}
+
+int main() {
+    std::cout << "Energia: " << Ciencia::Fisica::calcularEnergia(10) << std::endl;
+    return 0;
+}
