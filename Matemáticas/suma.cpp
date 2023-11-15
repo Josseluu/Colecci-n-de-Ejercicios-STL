78b642440ec6bbebca66a38bb95e09c488ea9d1f
Josseluu <jslslpz@myuax.com>
2023-11-15 18:55:00 +0100

    Initial commit

--- Matemáticas/suma.cpp
+++ Matemáticas/suma.cpp
@@ -2,14 +2,11 @@
 namespace Matematicas {
     int suma(int a, int b) {
         return a + b;
-    }
-    int resta(int a, int b) {
-        return a - b;
-    }
-}
+    }}
+
+
 
 int main() {
     std::cout << "Suma: " << Matematicas::suma(30, 3) << std::endl;
-    std::cout << "Resta: " << Matematicas::resta(20, 3) << std::endl;
      return 0;
 }
