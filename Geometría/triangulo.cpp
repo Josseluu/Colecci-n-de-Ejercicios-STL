36908edd525a27461580a6ad83ebdda2a313f7eb
Josseluu <jslslpz@myuax.com>
2023-11-15 18:45:37 +0100

    Initial commit

--- Geometría/triangulo.cpp
+++ Geometría/triangulo.cpp
@@ -4,3 +4,7 @@ namespace Geometria {
         return (base * altura) / 2;
     }
 }
+int main() {
+    std::cout << "Area del triangulo: " << Geometria::calcularAreaTriangulo(5, 3) << std::endl;
+    return 0;
+}
